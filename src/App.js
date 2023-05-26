f92b21ddf13242dbeaf47437946d12ca76d25c44
Priyanshu Pandey <priyanshu@aipalette.com>
2023-05-26 15:22:50 +0530

    Hotjar npm install

--- src/App.js
+++ src/App.js
@@ -8,6 +8,17 @@ import Modal from "@material-ui/core/Modal";
 import { Button, Avatar } from "@material-ui/core";
 import { Input } from "@material-ui/core";
 import ImageUpload from "./ImageUpload";
+import Hotjar from '@hotjar/browser';
+
+const siteId = 3508816;
+const hotjarVersion = 6;
+
+Hotjar.init(siteId, hotjarVersion);
+
+// Initializing with `debug` option:
+Hotjar.init(siteId, hotjarVersion, {
+  debug: true
+});
 
 function getModalStyle() {
   const top = 50;
